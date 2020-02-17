c3363c9c8fcdf741e08476a78772338cce9402de
Konstantin Iroshnikov <k.iroshnikov@itmegastar.com>
2020-02-17 20:45:33 +0700

    добавлен отдельный класс коллекции

---
+++ Sound recording/Views/CollectionTiks.swift
@@ -0,0 +1,62 @@
+//
+//  ViewTicks.swift
+//  Sound recording
+//
+//  Created by Username on 17.02.2020.
+//  Copyright © 2020 itMegastar. All rights reserved.
+//
+
+import UIKit
+
+class CollectionTiks: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        super.init(frame: .zero, collectionViewLayout: layout)
+
+        showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
+
+        backgroundColor = UIColor.clear
+        delegate = self
+        dataSource = self
+
+
+        self.baseSettingsCV(obj: self,
+                                      scrollEnabled: false,
+                                      clicableCell: false,
+                                      arrayNameCell: nil)
+        self.register(TickCell.self, forCellWithReuseIdentifier: "TickCell")
+
+
+        translatesAutoresizingMaskIntoConstraints = false
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+
+        layout.estimatedItemSize = CGSize(width: tickWidth, height: self.frame.height)
+
+
+        showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return Int(self.frame.width - 2 / tickWidth)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TickCell", for: indexPath) as! TickCell
+
+        cell.procent = CGFloat(arc4random_uniform(99))
+
+        return cell
+    }
+
+
+}
+
