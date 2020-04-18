91171345b580020a5e3ac3683f31ed2edddf9bd7
Hudihka <k.iroshnikov@itmegastar.com>
2020-04-18 19:37:51 +0700

    создание нового проекта

---
+++ v2.0/SoundRecords2/SoundRecords2/Resourses/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SoundRecords2
+//
+//  Created by Hudihka on 18/04/2020.
+//  Copyright © 2020 Tatyana. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+
+
+}
+
---
+++ v2.0/SoundRecords2/SoundRecords2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundRecords2
+//
+//  Created by Hudihka on 18/04/2020.
+//  Copyright © 2020 Tatyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
